2961ee005ba379dfa00cd1043496f63e03ec6a51
sainarenchava <S544921@NWMISSOURI.EDU>
2021-11-03 11:22:05 -0500

    query strings

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var TabletRouter = require('./routes/Tablet');
+var AddModsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Tablet', TabletRouter);
+app.use('/addmods', AddModsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
