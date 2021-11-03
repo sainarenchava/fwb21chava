256eb54c5b737d18ea213529a95e5d579869a67f
sainarenchava <S544921@NWMISSOURI.EDU>
2021-11-03 11:32:08 -0500

    selector endpoint added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var TabletRouter = require('./routes/Tablet');
 var AddModsRouter = require('./routes/addmods');
+var SelectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Tablet', TabletRouter);
 app.use('/addmods', AddModsRouter);
-
+app.use('/selector', SelectorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
